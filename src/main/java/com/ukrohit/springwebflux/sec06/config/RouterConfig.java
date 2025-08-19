6e3eded84c49bd5ddce651e41f41038e868cc761
Umesh Rohit <ukrohit678@gmail.com>
2025-08-19 23:32:19 +0530

    multiple route, inner route and filter

--- src/main/java/com/ukrohit/springwebflux/sec06/config/RouterConfig.java
+++ src/main/java/com/ukrohit/springwebflux/sec06/config/RouterConfig.java
@@ -22,13 +22,42 @@ public class RouterConfig {
     @Bean
     public RouterFunction<ServerResponse> customerRoutes() {
         return RouterFunctions.route()
+                .path("customer", this::customerRoutes2) // This is for nested router function
+                /* .GET("/customer", this.custmerRequestHandler::allCustomers)
+                 .GET("/customer/paginated", this.custmerRequestHandler::getCustomer)
+                 .GET("/customer/{id}", this.custmerRequestHandler::getCustomer)
+                */.POST("/customer", this.custmerRequestHandler::saveCustomer)
+                .PUT("/customer/{id}", this.custmerRequestHandler::updateCustomer)
+                .onError(CustomerNotFoundException.class, (ex, req) -> this.excveptionHandler.handleException(ex, req))
+                .onError(InvalidInutException.class, (ex, req) -> this.excveptionHandler.handleException(ex, req))
+                /*.filter(((request, next) -> {
+                    //return next.handle(request); order 1
+                    return ServerResponse.badRequest().build();
+                }))
+                .filter(((request, next) -> {
+                            //return next.handle(request);order 2
+                            return ServerResponse.badRequest().build();
+                        })
+                )*/
+
+
+                .build();
+    }
+
+    // @Bean
+  /*  public RouterFunction<ServerResponse> customerRoutes2() {
+        return RouterFunctions.route()
                 .GET("/customer", this.custmerRequestHandler::allCustomers)
                 .GET("/customer/paginated", this.custmerRequestHandler::getCustomer)
                 .GET("/customer/{id}", this.custmerRequestHandler::getCustomer)
-                .POST("/customer", this.custmerRequestHandler::saveCustomer)
-                .PUT("/customer/{id}", this.custmerRequestHandler::updateCustomer)
-                .onError(CustomerNotFoundException.class,(ex,req)->this.excveptionHandler.handleException(ex,req))
-                .onError(InvalidInutException.class,(ex, req)->this.excveptionHandler.handleException(ex,req))
+                .build();
+    }*/
+
+    public RouterFunction<ServerResponse> customerRoutes2() {
+        return RouterFunctions.route()
+                .GET("/paginated", this.custmerRequestHandler::getCustomer)
+                .GET("/{id}", this.custmerRequestHandler::getCustomer)
+                .GET("", this.custmerRequestHandler::allCustomers)
                 .build();
     }
 }
